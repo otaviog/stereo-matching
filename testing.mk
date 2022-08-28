bcf1626247b68f89404e6075e72262742ae84df7
Otavio Gomes <otavio.b.gomes@gmail.com>
2022-08-28 13:41:03 -0300

    Update app testing

--- testing.mk
+++ testing.mk
@@ -4,17 +4,15 @@ about:
 MAX_DISPARITY := 128
 
 ssd-wta-playroom:
-	python -m stereomatch.single_image_app test-data/middleburry/playroom/im0.png test-data/middleburry/playroom/im1.png 128 ssd-wta-playroom.pgm
+	stm-run-pair test-data/middleburry/playroom/im0.png test-data/middleburry/playroom/im1.png 128 ssd-wta-playroom.pgm
 
 ssd-wta-teddy:
-	python -m stereomatch.single_image_app\
-		test-data/middleburry/teddy/im2.png\
+	stm-run-pair test-data/middleburry/teddy/im2.png\
 		test-data/middleburry/teddy/im6.png\
 		${MAX_DISPARITY} ssd-wta-teddy.png -c
 
 ssd-texture-wta-teddy:
-	python -m stereomatch.single_image_app\
-		test-data/middleburry/teddy/im2.png\
+	stm-run-pair test-data/middleburry/teddy/im2.png\
 		test-data/middleburry/teddy/im6.png\
 		--cost-method ssd-texture\
 		--aggregation-method wta\
